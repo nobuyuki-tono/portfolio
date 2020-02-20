109a83ea74abb284cf28cf31920e99db8d85b10b
Nobu <rockmusic2090@gmail.com>
2020-02-19 17:00:38 -0800

    Added hambuger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,57 @@ import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <a href="#home">HOME</a>
-        </li>
-        <li>
-          <a href="#work">WORK</a>
-        </li>
-        <li>
-          <a href="#skill">SKILL</a>
-        </li>
-        <li>
-          <a href="#contact">CONTACT</a>
-        </li>
-        <li>
-          <a href="#contact">RESUME</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="navbar">
+        <ul>
+          <li>
+            <a href="#home">HOME</a>
+          </li>
+          <li>
+            <a href="#work">WORK</a>
+          </li>
+          <li>
+            <a href="#skill">SKILL</a>
+          </li>
+          <li>
+            <a href="#contact">CONTACT</a>
+          </li>
+          <li>
+            <a href="#contact">RESUME</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="menu-wrap">
+        <input type="checkbox" className="toggler" />
+        <div className="hamburger">
+          <div></div>
+        </div>
+        <div className="menu">
+          <div>
+            <div>
+              <ul>
+                <li>
+                  <a href="">HOME</a>
+                </li>
+                <li>
+                  <a href="">WORK</a>
+                </li>
+                <li>
+                  <a href="">SKILL</a>
+                </li>
+                <li>
+                  <a href="">CONTCT</a>
+                </li>
+                <li>
+                  <a href="">RESUME</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
